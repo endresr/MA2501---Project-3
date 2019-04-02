ca22ac4a34f51c8b63097e6e9041144fb6de7f2c
Endre Sørmo Rundsveen <endresr@stud.ntnu.no>
2019-04-02 10:09:05 +0200

    Fuc convention

--- IntegrationMethods.py
+++ IntegrationMethods.py
@@ -13,7 +13,7 @@ The following 2 functions,
 are in conjunction with task 1 a) in the project.
 """
 
-def Simpson(f,interval):
+def Simpson(f, interval):
     """
     This is the Simpson rule
     Inputs:
@@ -22,10 +22,10 @@ def Simpson(f,interval):
     Output:
         The estimated integration of f over the interval
     """
-    a,b=interval
+    a, b = interval
     return ((b-a)/6)*(f(a)+4*f((a+b)/2)+f(b))
     
-def adaptiveSimpson(f,interval,TOL):
+def adaptiveSimpson(f, interval, TOL):
     """
     This is the adaptive Simpson integration method
     Inputs:
@@ -33,23 +33,22 @@ def adaptiveSimpson(f,interval,TOL):
         Interval = a tuple with the start and ending point of the integration
         TOL = The bound we set upon the estimated error. 
     Output:
-        The estimated definite integral of f
-    
-    The error estimate is here given by the absolute difference between the 
+        The estimated definite integral of f    
+    The error estimate is here given by the absolute difference between the
     simpson rule over the given interval and the composite simpson rule with
     two subintervals over the same interval, divided by 15. 15 comes from the
     fact that the composite simpson has 1/m**2 better error bound than normal
     simpson where m is the number of subintervals.
     """
-    I1=Simpson(f,interval) #Simpsons Rule
-    a,b=interval
-    c=(a+b)/2
-    I2=Simpson(f,(a,c))+Simpson(f,(c,b)) #Composite Simpson with two subint
-    Error= (1/15)*np.abs(I2-I1)
-    if Error<TOL:
-        I2=I2+(1/15)*(I2-I1)
+    I1 = Simpson(f, interval) #Simpsons Rule
+    a, b = interval
+    c = (a+b)/2
+    I2 = Simpson(f, (a, c))+Simpson(f, (c, b)) #Composite Simpson with two subint
+    Error = (1/15)*np.abs(I2-I1)
+    if Error < TOL:
+        I2 = I2+(1/15)*(I2-I1)
     else:
-        I2=adaptiveSimpson(f,(a,c),TOL/2)+adaptiveSimpson(f,(c,b),TOL/2)
+        I2 = adaptiveSimpson(f, (a, c), TOL/2)+adaptiveSimpson(f, (c, b), TOL/2)
     return I2
 
 """
@@ -60,7 +59,7 @@ are in conjunction with task 1 c) of the project.
 """
 
 
-def rombergIntegration(f,interval,m,TOL):
+def rombergIntegration(f, interval, m, TOL):
     """
     This is the Romberg integration method
     Inputs:
@@ -75,32 +74,32 @@ def rombergIntegration(f,interval,m,TOL):
     """
     #Initializing
     #Variables
-    a,b=interval
-    hn=(b-a)
-    RombMatr=np.zeros((m,m))
-    RombMatr[0,0]=(1/2)*hn*(f(a)+f(b))
+    a, b = interval
+    hn = (b-a)
+    RombMatr = np.zeros((m, m))
+    RombMatr[0, 0] = (1/2)*hn*(f(a)+f(b))
     #Needed function
-    def errCor(n,k):
-        return (1/(4**(k)-1))*(RombMatr[n,k-1]-RombMatr[n-1,k-1])
+    def errCor(n, k):
+        return (1/(4**(k)-1))*(RombMatr[n, k-1]-RombMatr[n-1, k-1])
     #Main
-    for n in range(1,m):
-        addition=0
-        hn=hn*1/2
+    for n in range(1, m):
+        addition = 0
+        hn = hn*1/2
         for i in range(2**(n-1)):
-            addition+=f(a+(2*i+1)*hn)
-        RombMatr[n][0]=(1/2)*RombMatr[n-1,0]+hn*addition
-        for k in range(1,n):
-            RombMatr[n][k]=RombMatr[n,k-1]+errCor(n,k)
-        if n !=1: #Else the code will divide by zero
-            if np.abs(errCor(n,n-1))<TOL:
-                return RombMatr[n,n-1]
-    return RombMatr[-1,-1]
+            addition += f(a+(2*i+1)*hn)
+        RombMatr[n][0] = (1/2)*RombMatr[n-1, 0]+hn*addition
+        for k in range(1, n):
+            RombMatr[n][k] = RombMatr[n, k-1]+errCor(n, k)
+        if n != 1: #Else the code will divide by zero
+            if np.abs(errCor(n, n-1)) < TOL:
+                return RombMatr[n, n-1]
+    return RombMatr[-1, -1]
 
 """
 Implicit Midpoint Runge-Kutta 
 """
 
-def Newt(func,invJac,x0,t,NIter=100,TOL=1e-7):
+def Newt(func, invJac, x0, t, NIter=100, TOL=1e-7):
     """
     Newton method for finding the implicit functions in impMidRungKut
     Note that as its associated function, this function is hardcoded for
@@ -115,18 +114,18 @@ def Newt(func,invJac,x0,t,NIter=100,TOL=1e-7):
     Output:
         The fixed-point
     """
-    i=0
-    x1=x0
-    while i<NIter:
-        x2=x1-invJac(t,x1[0],x1[1],x1[2]).dot(func(t,x1[0],x1[1],x1[0]))
-        Fx01=func(t,x2[0],x2[1],x2[2])
-        if np.linalg.norm(Fx01)<TOL or np.linalg.norm(x2-x1)<TOL:
+    i = 0
+    x1 = x0
+    while i < NIter:
+        x2 = x1-invJac(t, x1[0], x1[1], x1[2]).dot(func(t, x1[0], x1[1], x1[0]))
+        Fx01 = func(t, x2[0], x2[1], x2[2])
+        if np.linalg.norm(Fx01) < TOL or np.linalg.norm(x2-x1) < TOL:
             return x2
-        x1=x2
-        i+=1
+        x1 = x2
+        i += 1
     return x2
 
-def impMidRungKut(Interval,InitVal,F,Step,invJac):
+def impMidRungKut(Interval, InitVal, F, Step, invJac):
     """
     This is the code for the implicit Midpoint Runge-Kutta method.
     Note that this is hardcoded for three variables, and uses Newton method
@@ -144,19 +143,19 @@ def impMidRungKut(Interval,InitVal,F,Step,invJac):
         The function at time t
     """
     
-    yn=[InitVal]
-    t=Interval[0]
-    while t<Interval[1]:
-        K1=Newt(F,invJac,yn[-1],t)
+    yn = [InitVal]
+    t = Interval[0]
+    while t < Interval[1]:
+        K1 = Newt(F, invJac, yn[-1], t)
         yn.append(yn[-1]+Step*K1)
-        t+=Step
+        t += Step
     return yn
 
 """Per 28.03.18 18:40 kan det se ut som det er oppgitt formelen for modified
 Euler der det står at vi skal implementere improved Euler. Har dermed begge 
 i det følgende. Ref. Süli og Mayers (s. 328)"""
 
-def modiEul(Interval,InitVal,F,Step):
+def modiEul(Interval, InitVal, F, Step):
     """
     This is the modified Euler method.
     Input:
@@ -168,15 +167,15 @@ def modiEul(Interval,InitVal,F,Step):
     Output:
         The function at time t
     """
-    tn=Interval[0]
-    yn=InitVal
-    while tn<Interval[1]:
-        ynhalf=yn+.5*Step*F(tn,yn)
-        yn+=Step*F(tn+.5*Step,ynhalf)
-        tn+=Step
+    tn = Interval[0]
+    yn = InitVal
+    while tn < Interval[1]:
+        ynhalf = yn+.5*Step*F(tn, yn)
+        yn += Step*F(tn+.5*Step, ynhalf)
+        tn += Step
     return yn
 
-def imprEul(Interval,InitVal,F,Step):
+def imprEul(Interval, InitVal, F, Step):
     """
     This is the improved Euler method.
     Input:
@@ -188,11 +187,11 @@ def imprEul(Interval,InitVal,F,Step):
     Output:
         The function at time t
     """
-    tn=Interval[0]
-    yn=InitVal
-    while tn<Interval[1]:
-        fn=F(tn,yn)
-        fn2=F(tn+Step,yn+Step*fn)
-        yn+=.5*Step*(fn+fn2)
-        tn+=Step
+    tn = Interval[0]
+    yn = InitVal
+    while tn < Interval[1]:
+        fn = F(tn, yn)
+        fn2 = F(tn+Step, yn+Step*fn)
+        yn += .5*Step*(fn+fn2)
+        tn += Step
     return yn
--- Tests.py
+++ Tests.py
@@ -9,11 +9,13 @@ import IntegrationMethods as IntM
 def test_adaptiveSimpson():
     f=lambda x: np.cos(2*np.pi*x)
     g=lambda x: np.exp(3*x)*np.sin(2*x)
-    gint=(1/13)*(2+3*np.exp((3*np.pi)/4))
+    gval=(1./13)*(2.+3.*np.exp((3*np.pi)/4))
+    print(gval)
+    print(IntM.adaptiveSimpson(g,(0,np.pi/4),1e-7))
     assert np.abs(0-IntM.adaptiveSimpson(f,(0,1),1e-7))<1e-7, \
     "adaptive Simpson Quadrature gives wrong value for f(x)=cos(2*pi*x) over\
     [0,1]"
-    assert np.abs(gint-IntM.adaptiveSimpson(g,(0,np.pi/4),1e-7))<1e-7, \
+    assert np.abs(gval-IntM.adaptiveSimpson(g,(0,np.pi/4),1e-7))<1e-7, \
     "adaptive Simpson Quadrature gives wrong value for g(x)=exp(3*x)*sin(2x)\
     over [0,pi/4]"
 test_adaptiveSimpson()
