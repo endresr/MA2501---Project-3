60f5b8e79e389b8027132c3a74cd02dea60dbd9c
Endre Sørmo Rundsveen <endresr@stud.ntnu.no>
2019-04-04 11:14:09 +0200

    Yolo 2. The Yoloing

--- IntegrationMethods.py
+++ IntegrationMethods.py
@@ -147,7 +147,12 @@ def impMidRungKut(Interval, InitVal, F, Step, Jac):
     yn = [InitVal]
     t = Interval[0]
     while t < Interval[1]:
-        K1 = Newt(F, Jac, yn[-1], t)
+        JacK=lambda t,K: np.diag((1,1,1))-Jac(t+Step/2,yn[-1]+Step/2*K)
+        print(JacK(1,np.ones((3,1))))
+        Fu=lambda t,K: K-F(t+Step/2,yn[-1]+Step/2*K)
+        print(Fu(1,np.ones((3,1))))
+        print(t)
+        K1 = Newt(Fu, JacK, np.zeros(3,1), t)
         yn.append(yn[-1]+Step*K1)
         t += Step
     return yn
